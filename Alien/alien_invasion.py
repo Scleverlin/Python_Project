f83dde6778e15d6b2da13330edda9f6461b66b3d
shi <shi@shi>
2023-06-14 00:39:57 +0900

    lazy_me

--- Alien/alien_invasion.py
+++ Alien/alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-
+import game_functions as gf
 from settings import Settings
 from ship import Ship
 def run_game():
@@ -9,17 +9,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     
     pygame.display.set_caption("Alien Invasion")# title for the windows
-    ship=Ship(screen) # make an instance of ship，and pass screen 
+    ship=Ship(ai_settings,screen) # make an instance of ship，and pass screen 
     
     while True :
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
+        gf.check_events(ship)
+        ship.update()
         # redraw the screen color during each pass through the loop
-        screen.fill(ai_settings.bg_color) # only takes one argument, a color
-        ship.blitme() # 
-        # redraw the screen during each pass through the loop
-        pygame.display.flip()
-        
+        gf.update_screen(ai_settings,screen,ship)
+
 run_game()
--- Alien/ship.py
+++ Alien/ship.py
@@ -2,19 +2,27 @@ import pygame
 
 class Ship:
     
-     def __init__ (self,screen):
+     def __init__ (self,ai_settings,screen):
          self.screen=screen
-         
+         self.ai_settings = ai_settings 
          # load the ship image and get its rect
          self.image=pygame.image.load('./ship.bmp') # load the image
          self.rect = self.image.get_rect()  # get_rect() method to access the surface's rect attribute
          self.screen_rect = screen.get_rect() 
+         self.center = float(self.rect.centerx)
          # start each new ship at the bottom center of the screen
-
+         self.rect.centerx = self.screen_rect.centerx # center the ship, x axis
+         self.rect.bottom = self.screen_rect.bottom   # bottom the ship  
+         self.moving_right = False
+         self.moving_left  = False 
          
+         self.ship_speed_factor_right = 1.1
+         self.ship_speed_factor_left = 0.1 # in my keaboard, the left key speed is faster than that of right key
+     def update(self):
+         if self.moving_right and self.rect.right < self.screen_rect.right :
+             self.rect.centerx += self.ship_speed_factor_right
+         if self.moving_left and self.rect.left > 0:  
+             self.rect.centerx -= self.ship_speed_factor_left
      def blitme(self): # bit block transfer (blit) draws the image to the screen
         # draw the ship at its current location  
-        self.rect.centerx = self.screen_rect.centerx # center the ship, x axis
-        self.rect.bottom = self.screen_rect.bottom   # bottom the ship  
-        
         self.screen.blit(self.image,self.rect)

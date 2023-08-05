cd09a133a88e0855e2b3bfde36bc7296bc11496c
shi <shi@shi>
2023-08-05 21:40:43 +0900

    1.3

--- python_exercise/pcost.py
+++ python_exercise/pcost.py
@@ -4,14 +4,10 @@
 f = open("portfolio.dat")
 
 listofdata=[]
-i=0
+
 for line in f:
     listofdata.append(line.split(' '))
 print (listofdata)
-
-for data in listofdata:
-  data[2].replace('\n','')
-  print(data[2])
 
 stockprice=0
 for data in listofdata:
